9afd2ee2aac1efce97ca0b0dc550fa5f061c4212
RAJESH BOYA <BOYARAJESH888@GMAIL.COM>
2026-01-13 18:32:40 +0530

    Create ThirdMaximumNumber.py

---
+++ ThirdMaximumNumber.py
@@ -0,0 +1,10 @@
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums=list(set(nums))
+        nums.sort()
+        if len(nums)==1:
+            return nums[0]
+        elif len(nums)==2:
+            return max(nums)
+        else:
+            return nums[-3]
